af7a5422fecb222715e1d3b5c473e76806cde30d
Will <willjordan2018@gmail.com>
2024-10-19 17:54:39 +0100

    Still building the base of the program

--- Together Culture CRM/MainWindow.xaml.cs
+++ Together Culture CRM/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Together_Culture_CRM
 {
@@ -24,5 +25,35 @@ namespace Together_Culture_CRM
         {
             InitializeComponent();
         }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void btnMaximise_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else WindowState = WindowState.Maximized;
+        }
+
+        private void btnMinimise_Click(object sender, RoutedEventArgs e)
+        {
+            //change the WindowStyle to single border just before minimising it
+            WindowStyle = WindowStyle.SingleBorderWindow;
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MainWindow_OnActivated(object sender, EventArgs e)
+        {
+            //change the WindowStyle back to None, but only after the Window has been activated
+            Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, new Action(() => WindowStyle = WindowStyle.None));
+        }
+
     }
 }
